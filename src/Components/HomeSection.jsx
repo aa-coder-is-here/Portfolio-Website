2a4fe3c2f315297d92e914f24ea1c5e9ed519949
Ali Arif <aliarif46807@gmail.com>
2025-03-16 12:10:12 +0500

    Responsive Done

--- src/Components/HomeSection.jsx
+++ src/Components/HomeSection.jsx
@@ -6,17 +6,22 @@ import Image from "next/image";
 
 const HomeSection = () => {
     return (
-        <div className="w-full h-screen relative overflow-hidden">
+        <div className="w-full h-[calc(100vh-70px)] lg:h-screen relative overflow-hidden">
 
-        <div className="w-full h-full flex items-center">
+          {/* Overlay Div */}
+          <div className="absolute right-0 lg:right-10 top-1 lg:top-12">
+            <BreathCircle />
+            </div>
+
+        <div className="w-full h-full flex flex-col lg:flex-row items-center relative z-2">
         
             {/* The first section is here */}
-            <div className="w-full h-full flex flex-col mt-88 items-end">
+            <div className="w-full h-full flex flex-col lg:mt-88 justify-center lg:justify-start lg:items-end items-center">
             
-            <div className="flex flex-col gap-0 leading-[113px]">
+            <div className="flex flex-col gap-0 lg:leading-[99px]">
                 {
                     MainHeading.map((curElem,index)=>{
-                        return <span key={index} className="text-[96px] font-[500]">
+                        return <span key={index} className="text-4xl lg:text-[80px] font-[500]">
                             {curElem}
                         </span>
                     })
@@ -27,12 +32,8 @@ const HomeSection = () => {
             {/* The Second pic section is here  */}
             <div className="w-full h-full relative flex items-end">
             
-             {/* Overlay Div */}
-            <div className="absolute right-10 top-12">
-            <BreathCircle />
-               </div>
-              <div className="relative z-2">
-                <Image className="grayscale" src={Mine} width={900} height = {900} alt="Mine Profile Pic" />
+              <div className="w-full h-full relative z-2">
+                <Image className="grayscale" src={Mine} fill style={{ objectFit: "cover" }} alt="Mine Profile Pic" />
               </div>
             </div>
         </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,10 +9,10 @@ import { useSidebar } from "./Context";
 const Navbar = () => {
     const {isOpen , setOpen} = useSidebar();
     return (
-        <div className="w-full h-20 fixed top-0 left-0 flex justify-center md:mt-6 z-10">
-        <div className="w-11/12 h-full md:rounded-full bg-[linear-gradient(0deg,#32323233,#64646433)] backdrop-blur-sm flex justify-between items-center md:px-8"> 
+        <div className="w-full h-20 fixed top-0 left-0 flex justify-center lg:mt-6 z-10">
+        <div className="w-full lg:w-11/12 h-full lg:rounded-full md:px-6 bg-[linear-gradient(0deg,#32323233,#64646433)] backdrop-blur-sm flex justify-between items-center lg:px-8"> 
         {/* Logo is here*/}
-        <div className="w-1/3 space-x-1 font-[900] md:text-4xl text-2xl">
+        <div className="w-1/3 space-x-1 font-[900] lg:text-4xl text-2xl">
             {
                 Logo.map((curElem,index)=>{
                     return <span key={index}>{curElem}</span>
@@ -21,7 +21,7 @@ const Navbar = () => {
         </div>
 
         {/* Nav Links are here for desktop */}
-        <div className="w-1/3 hidden md:flex justify-center gap-6">
+        <div className="w-1/3 hidden lg:flex justify-center gap-6">
             {
                 NavLinks.map((title,index)=>{
                     return <Link href="/" key={index} className="text-2xl font-[600]">{title}</Link>
@@ -34,7 +34,7 @@ const Navbar = () => {
             <HireButton text = {HeaderButton[0]} text2 = {HeaderButton[1]} />
 
             {/* Mobile Menu is */}
-            <div className="white md:hidden text-3xl rotate-180 cursor-pointer">
+            <div className="white lg:hidden text-3xl rotate-180 cursor-pointer">
                <CgMenuLeftAlt onClick={()=>{
                  setOpen(!isOpen);
                }} />
@@ -43,7 +43,7 @@ const Navbar = () => {
 
         </div>
         
-        <div className={`w-3/4 md:hidden h-screen absolute top-0 left-0 z-10 bg-[#171717] px-5 py-6 ${isOpen ? "block" : "hidden"}`}>
+        <div className={`w-3/4 lg:hidden h-screen absolute top-0 left-0 z-10 bg-[#171717] px-5 py-6 ${isOpen ? "block" : "hidden"}`}>
            
            <span className="text-3xl w-full flex justify-end"><RxCross1 onClick={()=>{
             setOpen(!isOpen);
